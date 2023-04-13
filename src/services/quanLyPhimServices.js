51527a0b52ccc7256de5178dee66eac57c64cde7
quoctue_ <87956563+quoctue29@users.noreply.github.com>
2023-04-13 08:58:47 +0700

    fixservice

--- src/services/quanLyPhimServices.js
+++ src/services/quanLyPhimServices.js
@@ -1,20 +1,19 @@
-import { api, MaNhom } from "../constants/api"
+import { api, MaNhom } from "../constants/api";
 
 export const quanLyPhimServices = {
-   layDanhSachBanner: () => {
-      return api.get('/api/QuanLyPhim/LayDanhSachBanner')
-   },
-   layDanhSachPhim: (keyword) => {
-      return api.get(`/api/QuanLyPhim/LayDanhSachPhim?maNhom=${MaNhom}&${keyword}`)
-   },
-   themPhimUploadHinh: (data) => {
-      return api.post(`/api/QuanLyPhim/ThemPhimUploadHinh`, data)
-   },
-   capNhatPhimUpload: (data) => {
-      return api.post(`/api/QuanLyPhim/CapNhatPhimUpload`, data)
-   },
-   xoaPhim: (maPhim) => {
-      return api.delete(`/api/QuanLyPhim/XoaPhim?MaPhim=${maPhim}`)
-   },
-
-}
+  layDanhSachBanner: () => {
+    return api.get("/api/QuanLyPhim/LayDanhSachBanner");
+  },
+  layDanhSachPhim: (keyword) => {
+    return api.get(`/api/QuanLyPhim/LayDanhSachPhim?maNhom=GP00&${keyword}`);
+  },
+  themPhimUploadHinh: (data) => {
+    return api.post(`/api/QuanLyPhim/ThemPhimUploadHinh`, data);
+  },
+  capNhatPhimUpload: (data) => {
+    return api.post(`/api/QuanLyPhim/CapNhatPhimUpload`, data);
+  },
+  xoaPhim: (maPhim) => {
+    return api.delete(`/api/QuanLyPhim/XoaPhim?MaPhim=${maPhim}`);
+  },
+};
